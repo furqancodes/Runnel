41bee4ce5edaa6640d077a7e5b6fc7395bb62f38
furqan_ashraf <furqan_ashraf@outlook.com>
2020-12-19 07:06:44 +0500

    implemented sgin-in sign-out button

--- client/src/components/GoogleAuth.jsx
+++ client/src/components/GoogleAuth.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 
 const GooleAuth = () => {
-  const { isSignedIn, setIsSignedIn } = useState(null);
+  const [isSignedIn, setIsSignedIn] = useState(null);
   const auth = useRef("");
   useEffect(() => {
     window.gapi.load("client:auth2", () => {
@@ -14,11 +14,44 @@ const GooleAuth = () => {
         .then(() => {
           auth.current = window.gapi.auth2.getAuthInstance();
           setIsSignedIn(auth.current.isSignedIn.get());
-          // auth.current.isSignedIn.listen()
+          auth.current.isSignedIn.listen(onAuthChnage);
         });
     });
-  }, [setIsSignedIn]);
-  return <div>Sign in by GOOGLE !</div>;
+  });
+  const onAuthChnage = () => {
+    setIsSignedIn(auth.current.isSignedIn.get());
+  };
+  const signInDecider = () => {
+    if (isSignedIn === null) {
+      return null;
+    }
+    if (isSignedIn) {
+      return (
+        <button
+          className="ui red google button"
+          onClick={() => {
+            auth.current.signOut();
+          }}
+        >
+          <i className="google icon" />
+          Sign Out
+        </button>
+      );
+    } else {
+      return (
+        <button
+          className="ui blue google button"
+          onClick={() => {
+            auth.current.signIn();
+          }}
+        >
+          <i className="google icon" />
+          Sign in
+        </button>
+      );
+    }
+  };
+  return <div>{signInDecider()}</div>;
 };
 export default GooleAuth;
 //
